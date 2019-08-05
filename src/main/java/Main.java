226c1cdb1eae6e604abdc778e613353e78231d42
Marcin Głąb <anrok1234@gmail.com>
2019-08-05 09:26:20 +0200

    - Adding book to database
    - Print book added to database
    
    - Update of gitignore

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,18 @@
+import dao.BookDao;
+import dao.BookDaoImpl;
+import model.Book;
+
 public class Main {
+    public static void main(String[] args) {
+
+        Book book = new Book("123498765", "Wilk z Wall Street", "Jordan Belfort");
+        BookDao bookDao = new BookDaoImpl();
+        bookDao.save(book);
+        System.out.println("Book saved");
+
+        System.out.println(bookDao.get(1L));
+        bookDao.cleanUp();
+    }
+
+
 }
