b82b177398b781ec29576dacd783c419cf319a53
Marcin Głąb <anrok1234@gmail.com>
2019-08-07 11:12:54 +0200

    - Set generation of KEY to IDENTITY - Key generated on database site ( no hibernate_sequence table )

--- src/main/java/App/model/Book.java
+++ src/main/java/App/model/Book.java
@@ -2,6 +2,7 @@ package App.model;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.io.Serializable;
 
@@ -11,7 +12,7 @@ public class Book implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String isbn;
     private String title;
