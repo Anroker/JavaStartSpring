92ea07fd88e6e1f1437d726df4c0ad298c073e3e
Marcin Głąb <anrok1234@gmail.com>
2019-08-07 11:00:43 +0200

    -Updated Main.java to use Spring Boot

--- src/main/java/App/Main.java
+++ src/main/java/App/Main.java
@@ -2,27 +2,32 @@ package App;
 
 import App.dao.BookDao;
 import App.model.Book;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
-@ComponentScan
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
 
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Main.class);
+        //AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Main.class);
+        ConfigurableApplicationContext contextBoot = SpringApplication.run(Main.class, args);
 
-        BookDao bookDao = context.getBean(BookDao.class);
-
-        Book book = new Book("123498765", "Wolf from Spring", "Jordan Belfort");
-        bookDao.save(book);
+        //BookDao bookDao = context.getBean(BookDao.class);
+        BookDao daoBoot = contextBoot.getBean(BookDao.class);
+        Book book = new Book("123498765", "Wolf from Spring Boot", "Jordan Belfort");
+        //bookDao.save(book);
+        daoBoot.save(book);
 
         System.out.println("Book saved");
 
-        System.out.println(bookDao.get(1L));
-
-        context.close();
+        //System.out.println(bookDao.get(1L));
+        System.out.println(daoBoot.get(1L));
+        //context.close();
+        contextBoot.close();
     }
 
 
