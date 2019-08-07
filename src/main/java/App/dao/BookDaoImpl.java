5659566d70a3da02f894bf23672ed78039f05ed3
Marcin Głąb <anrok1234@gmail.com>
2019-08-07 11:00:03 +0200

    - Added @Transacional to save method, cleaner code

--- src/main/java/App/dao/BookDaoImpl.java
+++ src/main/java/App/dao/BookDaoImpl.java
@@ -4,31 +4,20 @@ import App.model.Book;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.*;
+import javax.transaction.Transactional;
 
 @Repository
 public class BookDaoImpl implements BookDao {
 
-    @PersistenceUnit
-    private EntityManagerFactory managerFactory;
-    //private ;
-
-    public BookDaoImpl() {
-    }
-
-
+    @PersistenceContext
+    private EntityManager entityManager;
+    
+    @Transactional
     public void save(Book book) {
-        EntityManager entityManager = managerFactory.createEntityManager();
-        EntityTransaction entityTransaction = entityManager.getTransaction();
-        entityTransaction.begin();
         entityManager.persist(book);
-        entityTransaction.commit();
-        entityManager.close();
     }
 
     public Book get(Long id) {
-        EntityManager entityManager = managerFactory.createEntityManager();
-        Book book = entityManager.find(Book.class, id);
-        entityManager.close();
-        return book;
+        return entityManager.find(Book.class, id);
     }
 }
