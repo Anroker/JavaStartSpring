b73faec8fc4699f61980a76d33aae51ffc760c67
Marcin Głąb <anrok1234@gmail.com>
2019-08-07 11:56:43 +0200

    Update of BookDao
    - Renamed id to bookId in get method
    - Added CRUD methods to BookDao

--- src/main/java/App/dao/BookDao.java
+++ src/main/java/App/dao/BookDao.java
@@ -5,6 +5,8 @@ import App.model.Book;
 public interface BookDao {
 
     void save(Book book);
-    Book get(Long id);
+    Book get(Long bookID);
+    void update(Book book);
+    void delete(Long bookID);
 
 }
