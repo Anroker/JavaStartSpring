eba602735cbc5f3cfb27605dac25055d329fe988
Marcin Głąb <anrok1234@gmail.com>
2019-10-21 20:56:46 +0200

    Update Main.java
    Adding Client and order

--- src/main/java/App/Main.java
+++ src/main/java/App/Main.java
@@ -1,11 +1,7 @@
 package App;
 
-import App.dao.BookDao;
-import App.dao.UserDao;
-import App.dao.UserDetailsDao;
-import App.model.Book;
-import App.model.User;
-import App.model.UserDetails;
+import App.dao.*;
+import App.model.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -17,58 +13,74 @@ public class Main {
         //AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Main.class);
         ConfigurableApplicationContext contextBoot = SpringApplication.run(Main.class, args);
 
+        Client client = new Client("Marcin", "Glab","City 54, City");
+        ClientDao clientDao = contextBoot.getBean(ClientDao.class);
+        clientDao.save(client);
+        System.out.println(client);
+
+        Order order = new Order("LG TV", "Details of product");
+        order.setClient(client);
+        OrderDao orderDao = contextBoot.getBean(OrderDao.class);
+        orderDao.save(order);
+        System.out.println(order);
+
+        Order getOrder = orderDao.get(1L);
+        System.out.println("Get order: " + getOrder);
+
+        Client getClient = clientDao.get(1L);
+        System.out.println("Client:" + getClient);
         //BookDao bookDao = context.getBean(BookDao.class);
-        BookDao daoBoot = contextBoot.getBean(BookDao.class);
-        Book book = new Book("123498765", "Wolf from Spring Boot", "Jordan Belfort");
-        //bookDao.save(book);
-        daoBoot.save(book);
-
-
-        Book book1 = new Book("0987654321112", "New Book For Spring Boog", "Marcin Głąb");
-        book1.setId(1L);
-        daoBoot.update(book1);
-
-        Book findBook = daoBoot.get(1L);
-        System.out.println("Book that was get: " + findBook);
-
-        daoBoot.delete(1L);
-        Book book2 = daoBoot.get(1L);
-
-        System.out.println("Book that was removed: " + book2);
-
-        //context.close();
-
-
-
-        UserDao userDao = contextBoot.getBean(UserDao.class);
-        User user = new User("johnny234", "strongPass", "johnny@gmail.com");
-
-        userDao.save(user);
-
-
-        UserDetails details = new UserDetails("John", "Kowalski", "Krakowska 55, Warszawa");
-        user.setUserDetails(details);
-        userDao.update(user);
-
-
-        user.setPassword("passPass");
-        user.getUserDetails().setFirstName("Piotr");
-        userDao.update(user);
-
-
-        User userFromDb = userDao.get(1L);
-        System.out.println(userFromDb);
-
-
-       // userDao.remove(user);
-
-
-        userFromDb = userDao.get(1L);
-        System.out.println(userFromDb);
-
-        UserDetailsDao userDetailsDao = contextBoot.getBean(UserDetailsDao.class);
-        UserDetails userDetails = userDetailsDao.get(1L);
-        System.out.println("UserDetails: " + userDetails.getUser());
+//        BookDao daoBoot = contextBoot.getBean(BookDao.class);
+//        Book book = new Book("123498765", "Wolf from Spring Boot", "Jordan Belfort");
+//        //bookDao.save(book);
+//        daoBoot.save(book);
+//
+//
+//        Book book1 = new Book("0987654321112", "New Book For Spring Boog", "Marcin Głąb");
+//        book1.setId(1L);
+//        daoBoot.update(book1);
+//
+//        Book findBook = daoBoot.get(1L);
+//        System.out.println("Book that was get: " + findBook);
+//
+//        daoBoot.delete(1L);
+//        Book book2 = daoBoot.get(1L);
+//
+//        System.out.println("Book that was removed: " + book2);
+//
+//        //context.close();
+//
+//
+//
+//        UserDao userDao = contextBoot.getBean(UserDao.class);
+//        User user = new User("johnny234", "strongPass", "johnny@gmail.com");
+//
+//        userDao.save(user);
+//
+//
+//        UserDetails details = new UserDetails("John", "Kowalski", "Krakowska 55, Warszawa");
+//        user.setUserDetails(details);
+//        userDao.update(user);
+//
+//
+//        user.setPassword("passPass");
+//        user.getUserDetails().setFirstName("Piotr");
+//        userDao.update(user);
+//
+//
+//        User userFromDb = userDao.get(1L);
+//        System.out.println(userFromDb);
+//
+//
+//       // userDao.remove(user);
+//
+//
+//        userFromDb = userDao.get(1L);
+//        System.out.println(userFromDb);
+//
+//        UserDetailsDao userDetailsDao = contextBoot.getBean(UserDetailsDao.class);
+//        UserDetails userDetails = userDetailsDao.get(1L);
+//        System.out.println("UserDetails: " + userDetails.getUser());
     }
 
 
